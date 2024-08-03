e46631fcb967b10953a30c59724ab1e022206200
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-03 17:18:56 +0300

    feat: add lint and prettier config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const pluginReact = require("eslint-plugin-react");
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', '.next/**', 'build/**', 'eslint.config.js'],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+    }
+  }
+];
