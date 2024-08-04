ba975355194c74bff6df554c634385917d378819
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-04 16:02:12 +0300

    feat: add Section component

--- src/blocks/Hero/index.tsx
+++ src/blocks/Hero/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Hero as HeroType } from '@/payload-types';
 import { Block } from '@/utils/types';
+import { Section } from '@/components/Section';
 
 export type HeroProps = Block<HeroType>;
 
-export const Hero = (props: HeroProps) => {
-  console.log('props: ', props);
+export const Hero = ({
+  image,
+  heading,
+  subheading,
+  paddingTop,
+  paddingBottom,
+  breakpoints,
+}: HeroProps) => {
+  console.log('props: ', { image, heading, subheading });
 
-  return <div>Hero section WIP</div>;
+  return (
+    <Section paddingTop={paddingTop} paddingBottom={paddingBottom} breakpoints={breakpoints}>
+      <div className="container">Hero section WIP</div>
+    </Section>
+  );
 };
---
+++ src/components/Section/index.tsx
@@ -0,0 +1,61 @@
+import { HTMLAttributes, ReactNode, useId } from 'react';
+
+type SectionProps = HTMLAttributes<HTMLDivElement> & {
+  paddingTop?: number | null;
+  paddingBottom?: number | null;
+  breakpoints?:
+    | {
+        breakpoint?: number | null;
+        paddingTop?: number | null;
+        paddingBottom?: number | null;
+        id?: string | null;
+      }[]
+    | null;
+  children: ReactNode;
+};
+
+export const Section = ({
+  paddingTop,
+  paddingBottom,
+  breakpoints,
+  children,
+  ...rest
+}: SectionProps) => {
+  const id = useId();
+
+  const generateMediaQuery = (breakpoint: NonNullable<SectionProps['breakpoints']>[number]) => {
+    if (!breakpoint) return '';
+    const { breakpoint: maxWidth, paddingTop, paddingBottom } = breakpoint;
+    if (!maxWidth && paddingTop && paddingBottom) return '';
+
+    return `
+      @media screen and (max-width: ${maxWidth}px) {
+       [id="${id}"] {
+          padding-top: ${paddingTop}px;
+          padding-bottom: ${paddingBottom}px; 
+        }
+      }
+    `;
+  };
+
+  const initialStyles = `
+    [id="${id}"] {
+        padding-top: ${paddingTop}px;
+        padding-bottom: ${paddingBottom}px;
+    }
+  `;
+
+  return (
+    <section {...rest} id={id}>
+      {breakpoints && (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: initialStyles + breakpoints.map(generateMediaQuery).join('\n') || '',
+          }}
+        />
+      )}
+
+      {children}
+    </section>
+  );
+};
