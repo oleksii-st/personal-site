e329a058b4da6d449e07454ec98ff3eeb473d016
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-08 21:18:06 +0300

    feat: add redirects

--- src/graphql/redirects.ts
+++ src/graphql/redirects.ts
@@ -1,11 +1,28 @@
 export const REDIRECTS = `
-  query Redirects {
-     Redirects {
-        docs {
-               id
-               from
-               to
-           }
+      query {
+        Redirects {
+            docs {
+              id
+              from
+              to {
+                type
+                url
+                reference {
+                  relationTo
+                  value {
+                    ... on Page {
+                      slug
+                      id
+                      breadcrumbs {
+                        url
+                      }
+                    }
+                  }
+                }
+              }
+              updatedAt
+              createdAt
+          }
       }
   }
 `;
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,32 @@ export async function middleware(request: Request) {
   const url = new URL(request.url);
   const pathname = url.pathname;
 
-  const redirects = await fetchRedirects();
-  const currentRedirect = redirects.find((redirect) => redirect.from === pathname);
+  try {
+    const redirects = await fetchRedirects();
+    const currentRedirect = redirects.find(
+      (redirect) => redirect.from === pathname || redirect.from === request.url,
+    );
 
-  if (currentRedirect) {
-    return NextResponse.redirect(new URL(currentRedirect.to, request.url));
+    if (currentRedirect) {
+      if (currentRedirect.to?.type === 'reference') {
+        const value = currentRedirect.to.reference?.value;
+
+        if (typeof value === 'string') {
+          return NextResponse.redirect(new URL(value as string, request.url));
+        }
+
+        if (value && 'breadcrumbs' in value) {
+          return NextResponse.redirect(
+            new URL(String(value.breadcrumbs?.at(-1)?.url), request.url),
+          );
+        }
+      } else {
+        return NextResponse.redirect(new URL(currentRedirect.to?.url as string, request.url));
+      }
+    }
+  } catch (e: unknown) {
+    console.log(e);
+    return NextResponse.next();
   }
 
   return NextResponse.next();
