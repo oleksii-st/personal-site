4471a08cefcad107f415881a74b380961cac6f61
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-11 16:36:37 +0300

    feat: add hero block

--- src/blocks/Hero/index.tsx
+++ src/blocks/Hero/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Media } from '@/components/Media';
 import { Section } from '@/components/Section';
 import { Hero as HeroType } from '@/payload-types';
+import { cn } from '@/utils/cn';
 import { Block } from '@/utils/types';
 
 export type HeroProps = Block<HeroType>;
@@ -13,12 +15,45 @@ export const Hero = ({
   paddingTop,
   paddingBottom,
   breakpoints,
+  isFirst,
 }: HeroProps) => {
-  console.log('props: ', { image, heading, subheading });
+  const loading = isFirst ? 'eager' : 'lazy';
 
   return (
     <Section paddingTop={paddingTop} paddingBottom={paddingBottom} breakpoints={breakpoints}>
-      <div className="container">Hero section WIP</div>
+      <div className="container">
+        <div
+          className={cn(
+            'max-w-[500px] mx-auto flex flex-col gap-4',
+            'sm:max-w-full sm:flex-row sm:gap-8 sm:items-center',
+          )}
+        >
+          {image && (
+            <div className="sm:w-[calc(50%-16px)]">
+              <Media
+                className="w-full rounded-full my-0"
+                sizes="(max-width: 767px) calc(100vw - 32px), min(calc(50vw - 48px), 704px)"
+                source={image}
+                width={704}
+                height={704}
+                loading={loading}
+              />
+            </div>
+          )}
+
+          {Boolean(heading || subheading) && (
+            <div className={cn('text-center', 'sm:text-left sm:w-[calc(50%-16px)]')}>
+              {heading && (
+                <h1 className={cn('mb-4', 'sm:md-8 sm:text-5xl', 'md:text-7xl')}>{heading}</h1>
+              )}
+
+              {subheading && (
+                <p className={cn('m-0 text-xl', 'sm:text-2xl', 'md:text-4xl')}>{subheading}</p>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
     </Section>
   );
 };
--- src/components/Media/index.tsx
+++ src/components/Media/index.tsx
@@ -3,21 +3,32 @@ import Image, { ImageProps } from 'next/image';
 import { Media as MediaType } from '@/payload-types';
 
 type MediaProps = Omit<ImageProps, 'alt' | 'src'> & {
-  source: MediaType;
+  source: MediaType | string;
 };
 
-export const Media = ({ source: { alt, url, width, height }, ...rest }: MediaProps) => {
+export const Media = ({ source, ...rest }: MediaProps) => {
+  if (typeof source === 'string') {
+    return (
+      <Image
+        alt={rest.width?.toString() ?? ''}
+        src={source}
+        width={Number(rest.width ?? 1408)}
+        height={Number(rest.height ?? 1408)}
+      />
+    );
+  }
+
+  const { alt, url, width, height } = source;
+
   const src = `${process.env.NEXT_PUBLIC_CMS_URL}${url}`;
 
   return (
-    <div>
-      <Image
-        {...rest}
-        alt={alt}
-        src={src}
-        width={Number(rest.width ?? width)}
-        height={Number(rest.height ?? height)}
-      />
-    </div>
+    <Image
+      {...rest}
+      alt={alt}
+      src={src}
+      width={Number(rest.width ?? width)}
+      height={Number(rest.height ?? height)}
+    />
   );
 };
