fccdc3c712efa0edd571301fb906d6e04bb68fec
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-20 21:16:49 +0300

    feat: add 429 error to contact form

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,9 +5,36 @@ import { EmailContactTemplate } from '@/components/EmailContactTemplate';
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY as string);
 const authorEmail = process.env.EMAIL as string;
+const rateLimitMap = new Map();
+const limit = 5;
+const windowMs = 60 * 5000;
 
 export async function POST(request: NextRequest) {
   const CONTACT_SERVER_ERROR = `Temporary server error. Please try again letter or write directly to ${authorEmail}`;
+
+  const ip = request.ip;
+
+  if (ip) {
+    if (!rateLimitMap.has(ip)) {
+      rateLimitMap.set(ip, {
+        count: 0,
+        lastReset: Date.now(),
+      });
+    }
+
+    const ipData = rateLimitMap.get(ip);
+
+    if (Date.now() - ipData.lastReset > windowMs) {
+      ipData.count = 0;
+      ipData.lastReset = Date.now();
+    }
+
+    if (ipData.count >= limit) {
+      return Response.json({ message: `Too many request! Try 5 minutes later` }, { status: 429 });
+    }
+
+    ipData.count += 1;
+  }
 
   try {
     const { email, topic, name, message } = await request.json();
