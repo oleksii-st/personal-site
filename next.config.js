100e5a29286e1136c189bfc23009845573196121
Oleksii S <stokrockyioleksii@gmail.com>
2024-08-21 19:37:05 +0300

    feat: make image remotePatterns dynamic

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const cmsUrl = new URL(process.env.NEXT_PUBLIC_CMS_URL);
+
+const protocol = cmsUrl.protocol.substring(0, cmsUrl.protocol.length - 1);
+
 module.exports = {
   images: {
     deviceSizes: [360, 393, 414, 575, 767, 991, 1199, 1440, 1600, 1920],
     imageSizes: [328, 361, 382, 543, 735, 927, 1135, 1376, 1536, 1856, 462, 576, 696, 776, 936],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
+        protocol: protocol,
+        hostname: cmsUrl.hostname,
+        port: cmsUrl.port,
       },
     ],
   },
